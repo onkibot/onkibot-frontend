44a043bab1054af6810b67b47e2dfb8febaff949
Erlend Åmdal <erlend@aamdal.com>
2017-02-22 13:16:31 +0100

    Add basic login form

--- app/components/OnkibotApp.js
+++ app/components/OnkibotApp.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import OnOffButton from '../containers/OnOffButton';
+import LoginForm from '../forms/LoginForm';
 import { Card, CardHeader, CardActions, FlatButton } from 'material-ui';
 
 const OnkibotApp = () => (
-    <Card>
-        <CardHeader title="Onkibot" subtitle="The button" />
-        <CardActions>
-            <OnOffButton />
-        </CardActions>
-    </Card>
+    <div>
+        <Card>
+            <CardHeader title="Onkibot" subtitle="The button" />
+            <CardActions>
+                <OnOffButton />
+            </CardActions>
+        </Card>
+        <Card>
+            <CardHeader title="Onkibot" subtitle="The login form" />
+            <CardActions>
+                <LoginForm />
+            </CardActions>
+        </Card>
+    </div>
 );
 
 export default OnkibotApp;
---
+++ app/forms/LoginForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { RaisedButton } from 'material-ui';
+import { TextField } from 'redux-form-material-ui';
+
+const LoginForm = (props) => {
+    const { handleSubmit } = props;
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <Field name="username" component={TextField} floatingLabelText="Username" />
+            </div>
+            <div>
+                <Field name="password" component={TextField} floatingLabelText="Password" type="password" />
+            </div>
+            <div>
+                <RaisedButton label="Log in" type="submit" />
+            </div>
+        </form>
+    );
+};
+
+export default reduxForm({
+    form: 'login',
+    onSubmit: (values) => {
+        console.log(values);
+    }
+})(LoginForm);
