2e12cdec36bdeba15b518027b388b4a5f090255d
Erlend Åmdal <erlend@aamdal.com>
2017-02-22 14:14:37 +0100

    Add and setup react-router and react-router-redux

--- app/entry.js
+++ app/entry.js
@@ -3,18 +3,30 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import onkibotApp from './reducers';
-import OnkibotApp from './components/OnkibotApp';
+import App from './components/App';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Router, Route, hashHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
 
 injectTapEventPlugin();
 
 const store = createStore(onkibotApp);
+const history = syncHistoryWithStore(hashHistory, store)
+
+const TestComponent = () => (
+    <div>Test</div>
+);
 
 render(
-	<MuiThemeProvider>
-		<Provider store={store}>
-			<OnkibotApp />
-		</Provider>
-	</MuiThemeProvider>
+    <Provider store={store}>
+        <MuiThemeProvider>
+            <Router history={history}>
+                <Route path="/" component={App}>
+                    <Route path="login" component={TestComponent} />
+                </Route>
+            </Router>
+        </MuiThemeProvider>
+    </Provider>
 , document.querySelector('#root'));
