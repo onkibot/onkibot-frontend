c14dc1040cb1705514fc4acf57a4b235ea6a86a1
Erlend Åmdal <erlend@aamdal.com>
2017-04-21 11:49:56 +0200

    Add external resource reducer (#39)

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -8,6 +8,7 @@ import courses from './courses';
 import categories from './categories';
 import resources from './resources';
 import addedExternalResources from './addedExternalResources';
+import externalResources from './externalResources';
 import users from './users';
 import userSearch from './userSearch';
 
@@ -18,6 +19,7 @@ const onkibotApp = combineReducers({
     categories,
     resources,
     addedExternalResources,
+    externalResources,
     users,
     userSearch,
     form: formReducer,
