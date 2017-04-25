57023526c4155da23c9c74b98f95e122c0f4b647
Erlend Åmdal <erlend@aamdal.com>
2017-04-25 13:18:06 +0200

    Add check mark for resources with my feedback (#43)

--- app/components/ResourceListItem.js
+++ app/components/ResourceListItem.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { ListItem } from 'material-ui';
 import LightBulb from 'material-ui/svg-icons/action/lightbulb-outline';
+import Done from 'material-ui/svg-icons/action/done';
 
-const ResourceListItem = ({ name, body }) => (
-  <ListItem primaryText={name} secondaryText={body} leftIcon={<LightBulb />} />
+const ResourceListItem = ({ name, body, hasMyFeedback }) => (
+  <ListItem
+    primaryText={name}
+    secondaryText={body}
+    leftIcon={<LightBulb />}
+    rightIcon={hasMyFeedback ? <Done /> : null}
+  />
 );
 
 ResourceListItem.propTypes = {
     name: React.PropTypes.string.isRequired,
-    body: React.PropTypes.string.isRequired
+    body: React.PropTypes.string.isRequired,
+    hasMyFeedback: React.PropTypes.bool.isRequired
 };
 
 export default ResourceListItem;
